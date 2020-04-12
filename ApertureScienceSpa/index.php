f47305baa0857d2d6bd1252f60bc0cc774b48ca0
Santiago lucero <62676003+Santiagoezequiellucero@users.noreply.github.com>
2020-04-12 02:10:39 -0400

    Add files via upload

---
+++ ApertureScienceSpa/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <meta charset="UTF-8">
+        <link href="css/estilos1.css" rel="stylesheet" type="text/css"/>
+        <title>Pistola de portales</title>
+        
+        
+    </head>
+    
+    <body>
+        <div id="divH">
+       
+    <center>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Aperture_Science.svg/768px-Aperture_Science.svg.png" 
+             height="150px" width="150px"/>
+    </center>  
+    
+        <header>
+            <h1 id="header1" align="center">
+                Aperture science spa
+                <hr />
+            </h1>
+        </header>
+            
+        </div>
+        <nav align="center">
+            <ul id="nav1">
+                <li>Ventas</li>   <li>Catalogo</li>    <li>Herramientas</li>
+            </ul>
+        </nav>
+       
+        <aside>
+            <ul id="aside">
+                <li id="asi1">Aside1</li><li>Aside2</li><li>Aside3</li><li>Aside4</li>
+                <li>Aside5</li><li>Aside6</li><li>Aside7</li>
+            </ul>
+        
+        </aside>
+           
+        
+        <section>
+            <h2>
+                Esta es la seccion de esta pagina <br />la que deberia contener
+                informacion o cosas
+            </h2>
+            <left>
+                <img src="https://whatsup.es/wp-content/uploads/2018/10/comparativo_de_good.jpg"/>
+            </left>
+        </section>
+        <br />
+        <br />
+        <br />
+        <br />
+    
+    <footer align="center">
+        
+        <h3 id="foot">
+            Contacto (+56) 912345678
+        </h3>
+    </footer>
+    
+        
+    </body>
+</html>
